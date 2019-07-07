c991dcfaadfd678cc3389ed9acf18e351e335a0a
suman <suman.maasdishetti@gmail.com>
2019-07-07 16:32:27 +0530

    Blog func and polish

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
 	pubdate = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
+
+
+	def pub_date(self):
+		return self.pubdate.strftime('%b %e %Y')
